356a863019331226cd09102142421c26ea30d627
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2023-12-09 15:36:04 +0530

    created django app

--- react-frontend/src/components/form.js
+++ react-frontend/src/components/form.js
@@ -14,7 +14,7 @@ function InputForm(){
     const predictBtn = () => {
         axios.default.headers.common['Content-Type'] = 'application/json';
         axios.default.headers.common['Accept'] = 'application/json' ;
-        axios.post('')
+        axios.post('http://127.0.0.1:8000/');
      }
 
 
